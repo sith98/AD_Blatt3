ab6f2c9897de973063f7d6d664d446d52f1c098d
sith98 <sith98@web.de>
2018-10-22 12:55:39 +0200

    Aufgabe 4: Added Quick Sort implementation

--- src/Aufgabe4.java
+++ src/Aufgabe4.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Aufgabe4 {
     private static final int[] exampleArray = {-5, 13, -32, 7, -3, 17, 23, 12, -35, 19};
@@ -54,6 +55,49 @@ public class Aufgabe4 {
         System.out.println(Arrays.toString(numbers));
     }
 
+    private static Random random = new Random();
+
+    private static void quickSort(int[] numbers) {
+        quickSort(numbers, 0, numbers.length - 1);
+    }
+
+    private static void quickSort(int[] numbers, int start, int end) {
+        if (end - start < 1) {
+            return;
+        }
+        int pivotIndex = random.nextInt(end - start) + start;
+        int pivot = numbers[pivotIndex];
+        // move pivot to the start
+        swap(numbers, start, pivotIndex);
+        int endSmaller = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (numbers[i] < pivot) {
+                endSmaller += 1;
+                swap(numbers, i, endSmaller);
+            }
+        }
+        // put pivot at the right place
+        swap(numbers, start, endSmaller);
+        quickSort(numbers, start, endSmaller - 1);
+        quickSort(numbers, endSmaller + 1, end);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+//    private static int[] help;
+//    private static void mergeSort(int[] numbers) {
+//        help = new int[numbers.length];
+//        mergeSort(numbers, 0, numbers.length - 1);
+//    }
+//
+//    private static void mergeSort(int[] numbers, int start, int end) {
+//        if (end - start < 1) {
+//            return;
+//        }
+//        int half = (start + end) / 2;
+//        mergeSort(numbers, 0, half);
+//        mergeSort(numbers, half + 1, end);
+//    }
+
     public static void main(String[] args) {
         System.out.println("Bubble sort:");
         bubbleSort(getExampleArray());
@@ -61,6 +105,8 @@ public class Aufgabe4 {
         insertionSort(getExampleArray());
         System.out.println("Selection sort:");
         selectionSort(getExampleArray());
+        System.out.println("Quick sort:");
+        quickSort(getExampleArray());
     }
 
 
